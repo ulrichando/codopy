27908711788528570bd22a04dbf9bc35cec1fdbc
ulrichando <ulrichando007@gmail.com>
2024-02-04 13:56:42 -0500

    update

--- src/components/common/Comp.tsx
+++ src/components/common/Comp.tsx
@@ -2,20 +2,21 @@ import "../../styles/Content.css";
 
 const Comp = () => {
   return (
-    <div className="wrapper">
-      <section className="slide-one relative">
+    <div className="wrapper relative flex">
+      <section className="slide-one w-screen h-screen flex items-center justify-around">
         <div className="container">
           <div className="circle blue">
             <div className="mini-circle"></div>
           </div>
-          <div className="container-text">
-            <div className="header flex flex-wrap top-50 left-20  ">
-              <h1 className=" text-7xl font-bold">Hello.</h1>
-              <p>
-                Welcome to my portfolio, <br /> explore and discover
-              </p>
+          <div className="flex">
+            <div className="absolute left-10 top-1/2 transform -translate-y-1/2 ml-60">
+              <div className="flex flex-col">
+                <h1 className="text-8xl font-bold ">Hello.</h1>
+                <p className="text-2sm mt-2">
+                  Welcome to my portfolio, <br /> explore and discover
+                </p>
+              </div>
             </div>
-
             <div className="circle green">
               <div className="mini-circle"></div>
             </div>

95d90bb4801014ed642f3c481a9d36031f5561f8
ulrichando <ulrichando007@gmail.com>
2024-02-04 13:56:35 -0500

    update

--- src/components/common/Comp.tsx
+++ src/components/common/Comp.tsx
@@ -3,13 +3,13 @@ import "../../styles/Content.css";
 const Comp = () => {
   return (
     <div className="wrapper">
-      <section className="slide-one">
+      <section className="slide-one relative">
         <div className="container">
           <div className="circle blue">
             <div className="mini-circle"></div>
           </div>
-          <div className="">
-            <div className="absolute top-90 left-80 ml-20  ">
+          <div className="container-text">
+            <div className="header flex flex-wrap top-50 left-20  ">
               <h1 className=" text-7xl font-bold">Hello.</h1>
               <p>
                 Welcome to my portfolio, <br /> explore and discover

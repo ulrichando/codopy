db2bab52091677180d13f5476bf5350619ebd89f
ulrichando <ulrichando007@gmail.com>
2024-02-07 14:10:04 -0500

    update

--- src/components/common/Sections.tsx
+++ src/components/common/Sections.tsx
@@ -1,13 +1,9 @@
-import "../../styles/Content.scss";
-
 const Sections = () => {
   return (
     <div className="wrapper">
       <section></section>
       <section></section>
-      <section className="section-3">
-        <div className="circle"></div>
-      </section>
+      <section className="section-3"></section>
     </div>
   );
 };

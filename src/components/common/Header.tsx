89f00638184145bc6dfdb9500c0af3f8d24207a7
Ulrichando <ulrlichando007@gmail.com>
2024-07-28 23:10:13 -0400

    udated

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,6 +10,8 @@ import "../../App.css";
 //   { name: "Hello", to: "/" },
 //   { name: "Get in touch", to: "/contact" },
 // ];
+
+// this is a test
 const navigation = [
   {
     name: "Projects",

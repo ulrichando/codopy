b5148ecb85eea0290a909ef5fa0afd231636d583
Ulrichando <ulrlichando007@gmail.com>
2024-11-09 16:25:34 -0500

    update

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -1,3 +1,30 @@
+// import { useState, useEffect } from "react";
+
+// const useDarkMode = (): [boolean, () => void] => {
+//   const [darkMode, setDarkMode] = useState(
+//     localStorage.getItem("darkmode") === "active"
+//   );
+
+//   useEffect(() => {
+//     if (darkMode) {
+//       document.body.classList.add("darkmode");
+//     } else {
+//       document.body.classList.remove("darkmode");
+//     }
+//   }, [darkMode]);
+
+//   const toggleDarkMode = () => {
+//     setDarkMode((prevDarkMode) => {
+//       const newDarkMode = !prevDarkMode;
+//       localStorage.setItem("darkmode", newDarkMode ? "active" : "null");
+//       return newDarkMode;
+//     });
+//   };
+
+//   return [darkMode, toggleDarkMode];
+// };
+
+// export default useDarkMode;
 import { useState, useEffect } from "react";
 
 const useDarkMode = (): [boolean, () => void] => {

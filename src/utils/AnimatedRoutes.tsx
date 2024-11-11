f54ff16b8fe0917966f505a8b1194464fd65a7c7
Ulrichando <ulrlichando007@gmail.com>
2024-11-11 03:54:20 -0500

    update

--- src/utils/AnimatedRoutes.tsx
+++ src/utils/AnimatedRoutes.tsx
@@ -2,14 +2,13 @@ import Homepage from "../pages/Homepage";
 import AboutPage from "../pages/AboutPage";
 import WorkPage from "../pages/WorkPage";
 import ContactPage from "../pages/ContactPage";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 function AnimatedRoutes() {
-  const location = useLocation();
   return (
     <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
+      <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/work" element={<WorkPage />} />

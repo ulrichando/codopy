e9aefb334f157c0e24a06d6318c6f76193826fe5
Ulrichando <ulrlichando007@gmail.com>
2025-01-06 12:35:16 -0500

    footer size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,14 +34,14 @@ const Footer: React.FC = () => {
 
   return (
     <section>
-      <footer className="fixed flex justify-between items-center bottom-0 w-full py-6 z-50">
+      <footer className="fixed flex justify-between items-center bottom-0 w-full py-6 md:py-10 z-50">
         <div className="flex justify-center items-center">
           <div className="absolute flex justify-center items-center gap-4 transform -translate-x-1/2 left-1/2">
             {navFooter.map((item) => (
               <button
                 key={item.name}
                 onClick={() => handleNavigation(item.index)}
-                className={`px-4 py-2 font-medium text-sm -mx-2 md:text-xl menuUnderline hover-underline ${
+                className={`px-4 py-2  font-medium text-sm -mx-2 md:text-xl menuUnderline hover-underline ${
                   currentPage === item.index ? "active-underline" : ""
                 }`}
               >

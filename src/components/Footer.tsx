319ce2f08ca332c8a4b3fa6789beffcca375337d
Ulrichando <ulrlichando007@gmail.com>
2025-01-06 01:36:39 -0500

    update footer hight

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer: React.FC = () => {
 
   return (
     <section>
-      <footer className="fixed flex justify-between items-center bottom-0 w-full py-10 z-50">
+      <footer className="fixed flex justify-between items-center bottom-0 w-full py-6 z-50">
         <div className="flex justify-center items-center">
           <div className="absolute flex justify-center items-center gap-4 transform -translate-x-1/2 left-1/2">
             {navFooter.map((item) => (
